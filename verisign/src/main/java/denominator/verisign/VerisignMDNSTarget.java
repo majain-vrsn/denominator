371b7893ae2888cdb7d83fad72d5ff3e4d641826
Hasika Pamunuwa <hpamunuwa@verisign.com>
2015-09-24 16:22:03 -0400

    Fix reading credentials

--- verisign/src/main/java/denominator/verisign/VerisignMDNSTarget.java
+++ verisign/src/main/java/denominator/verisign/VerisignMDNSTarget.java
@@ -1,10 +1,12 @@
 package denominator.verisign;
 
+import static denominator.common.Preconditions.checkNotNull;
 import static feign.Util.UTF_8;
 import static java.lang.String.format;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Map;
 
 import javax.inject.Inject;
 
@@ -66,14 +68,31 @@ class VerisignMDNSTarget implements Target<VerisignMDNS> {
   @Override
   public Request apply(RequestTemplate in) {
     in.insert(0, url());
-    List<Object> creds = ListCredentials.asList(credentials.get());
-    String xml =
-        format(SOAP_TEMPLATE, System.currentTimeMillis(), creds.get(0).toString(), creds.get(1)
-            .toString(), new String(in.body(), UTF_8));
+    
+    String username;
+    String password;
+    
+    Credentials creds = credentials.get();
+    if (creds instanceof List) {
+      @SuppressWarnings("unchecked")
+      List<Object> listCreds = (List<Object>) creds;
+      username = listCreds.get(0).toString();
+      password = listCreds.get(1).toString();
+    } else if (creds instanceof Map) {
+      @SuppressWarnings("unchecked")
+      Map<String, Object> mapCreds = (Map<String, Object>) creds;
+      username = checkNotNull(mapCreds.get("username"), "username").toString();
+      password = checkNotNull(mapCreds.get("password"), "password").toString();
+    } else {
+      throw new IllegalArgumentException("Unsupported credential type: "+ creds);
+    }
+    
+    String xml = format(SOAP_TEMPLATE, System.currentTimeMillis(), username, password, new String(in.body(), UTF_8));
     in.body(xml);
     in.header("Host", URI.create(in.url()).getHost());
     in.header("Content-Type", "application/soap+xml");
     return in.request();
   }
+  
 
 }
